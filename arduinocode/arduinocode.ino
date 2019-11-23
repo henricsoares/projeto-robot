e13a25c510977c5366a54273d4d674f52f4b7425
henricsoares <56280902+henricsoares@users.noreply.github.com>
2019-11-23 11:42:46 -0300

    Código para Arduino.

---
+++ arduinocode/arduinocode.ino
@@ -0,0 +1,26 @@
+#include <VarSpeedServo.h>
+const int num = 4;
+VarSpeedServo servos[num];  
+char rec[num]={};
+int pins[num] = {13,12,11,10};
+int angs[num] = {60,60,60,60};
+boolean aux[num]={};   
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  for (int i = 0; i <num; ++i){
+  
+    servos[i].attach(pins[i]);
+    servos[i].write(angs[i],20,false);
+    
+    }
+  
+  }
+
+void loop() {
+
+  comunica();
+    
+  }
---
+++ arduinocode/functions.ino
@@ -0,0 +1,96 @@
+void comunica(){
+
+  while(!Serial.available()){
+
+    Serial.println("Digite 'a' para autoteste, 'b' para programar os servos, 'c' para informar posições atuais:");  
+    delay(1500);   
+    }
+  
+    
+  if (Serial.available()){
+    
+    
+    char received = Serial.read();
+
+    //if(Serial.available()){
+      
+      
+      
+      switch(received){
+      
+        case('a'):
+      
+          Serial.println("autotestex'");
+          testa();
+          
+        
+        break;
+      
+        case('b'):
+
+          delay(1000);
+          for(int i = 0; i<num; ++i){
+            
+            angs[i] = Serial.parseInt();
+            Serial.println(angs[i]);
+                
+            }
+          goservo();  
+        break;
+
+        case('c'):
+      
+          Serial.print(angs[0]);
+          Serial.print(", ");
+          Serial.print(angs[1]);
+          Serial.print(", ");
+          Serial.print(angs[2]);
+          Serial.print(", ");
+          Serial.print(angs[3]);
+          Serial.println(";");
+          
+      
+        break;
+
+        default:
+        break;
+        }
+      //}
+    }
+  }
+
+void testa(){
+  
+  for (int i = 0; i<num; ++i){
+    
+    servos[i].write(100,20,true);
+    servos[i].write(60,20,true);
+    
+    } 
+  }
+
+
+void goservo(){
+  for(int i=0;i<num;++i){
+    angs[i] = constrain(angs[i],45,115);
+    servos[i].write(angs[i],20,false);
+    
+    }
+  Serial.print(angs[0]);
+  Serial.print(", ");
+  Serial.print(angs[1]);
+  Serial.print(", ");
+  Serial.print(angs[2]);
+  Serial.print(", ");
+  Serial.print(angs[3]);
+  Serial.println(";");
+}
+
+
+
+void LimparBuffer(){
+  
+  while(Serial.available() > 0){
+    Serial.read();
+    }
+  }
